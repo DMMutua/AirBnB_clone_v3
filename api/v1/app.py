ee19f9bc8208cf5c0444217d6031dd3d26cd95cb
DMMutua <lengowriter99@gmail.com>
2023-02-16 14:56:49 +0300

    error 404 jsonified and stats

--- api/v1/app.py
+++ api/v1/app.py
@@ -4,7 +4,7 @@
 
 
 from api.v1.views import app_views
-from flask import Flask
+from flask import Flask, make_response, jsonify
 from models import storage
 from os import environ
 
@@ -17,6 +17,10 @@ def close_storage(exception):
     """Closes Database and Other Persistent Storage Amenities"""
     storage.close()
 
+@app.errorhandler(404)
+def res_not_found(error):
+    """404 Error Response Output"""
+    return make_response(jsonify({'error': "Not found"}), 404)
 
 if __name__ == "__main__":
     """Import Guard; main function"""
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,6 +2,14 @@
 """Index of the API blueprint"""
 
 
+import models
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
+from models import storage
 from api.v1.views import app_views
 from flask import jsonify
 
@@ -10,3 +18,26 @@ from flask import jsonify
 def status():
     """Function that returns the Status of the API"""
     return jsonify({"status": "OK"})
+
+@app_views.route('/stats', strict_slashes=False, methods=['GET'])
+def object_stats():
+    """Retrieval of Object Number Stats by Type. Displays jsonified content"""
+
+
+    from models.amenity import Amenity
+    from models.city import City
+    from models.place import Place
+    from models.review import Review
+    from models.state import State
+    from models.user import User
+    from models import storage
+
+
+    classes = [Amenity, City, Place, Review, State, User]
+    names = ["amenities","cities","places","reviews","states","users"]
+
+    no_objs = {}
+    for i in range(len(classes)):
+        no_objs[names[i]] = storage.count(classes[i])
+
+    return jsonify(no_objs)
